8aa92ba66a1bd5347c947b049d31b0e954f117b5
Robin Bourianes <robin.bourianes@gmail.com>
2024-05-24 12:09:46 +0200

    wip: adding ci to build container, based on kash

---
+++ scripts/build_app.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
+. "$THIS_DIR/kash/kash.sh"
+
+## Parse options
+##
+
+PUBLISH=false
+CI_STEP_NAME="Build app"
+while getopts "pr:" option; do
+    case $option in
+        p) # publish app
+            PUBLISH=true
+            ;;
+        r) # report outcome to slack
+            CI_STEP_NAME=$OPTARG
+            load_env_files "$WORKSPACE_DIR/development/common/SLACK_WEBHOOK_SERVICES.enc.env"
+            trap 'slack_ci_report "$ROOT_DIR" "$CI_STEP_NAME" "$?" "$SLACK_WEBHOOK_SERVICES"' EXIT
+            ;;
+        *)
+            ;;
+    esac
+done
+
+## Init workspace
+##
+
+init_lib_infos "$ROOT_DIR"
+
+APP=$(get_json_value "$ROOT_DIR/package.json" "name")
+VERSION=$(get_json_value "$ROOT_DIR/package.json" "version")
+GIT_TAG=$(get_git_tag "$ROOT_DIR")
+
+load_env_files "$WORKSPACE_DIR/development/common/kalisio_dockerhub.enc.env"
+load_value_files "$WORKSPACE_DIR/development/common/KALISIO_DOCKERHUB_PASSWORD.enc.value"
+
+## Build container
+##
+
+IMAGE_NAME="$KALISIO_DOCKERHUB_URL/kalisio/$APP"
+IMAGE_TAG=latest
+if [[ -n "$GIT_TAG" ]]; then
+    IMAGE_TAG=$VERSION
+fi
+
+begin_group "Building container $IMAGE_NAME:$IMAGE_TAG ..."
+
+docker login --username "$KALISIO_DOCKERHUB_USERNAME" --password-stdin "$KALISIO_DOCKERHUB_URL" < "$KALISIO_DOCKERHUB_PASSWORD"
+# DOCKER_BUILDKIT is here to be able to use Dockerfile specific dockerginore (app.Dockerfile.dockerignore)
+DOCKER_BUILDKIT=1 docker build \
+    -t "$IMAGE_NAME:$IMAGE_TAG" \
+    "$ROOT_DIR"
+
+if [ "$PUBLISH" = true ]; then
+    docker push "$IMAGE_NAME:$IMAGE_TAG"
+fi
+
+docker logout "$KALISIO_DOCKERHUB_URL"
+
+end_group "Building container $IMAGE_NAME:$IMAGE_TAG ..."
